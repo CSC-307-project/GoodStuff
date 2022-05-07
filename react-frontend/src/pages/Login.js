57d0e3acb2398d23c4a72c390fdecb2f62c84058
Ramon <torres28ramon@gmail.com>
2022-05-07 12:40:55 -0700

    Added handleChange function, person object, and login function

--- react-frontend/src/pages/Login.js
+++ react-frontend/src/pages/Login.js
@@ -1,6 +1,7 @@
 import { textAlign } from "@mui/system";
 import React from "react";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const styles = {
   color: "blue",
@@ -9,8 +10,28 @@ const styles = {
   textAlign: "center"
 };
 
-const Login = () => {
+const Login = (props) => {
   window.scrollTo(0, 0);
+
+  function handleChange(event) {
+    console.log(person);
+    const { name, value } = event.target;
+    if (name === "password") setPerson({ email: person["email"], password: value, username: person["username"] });
+    else setPerson({ email: value, password: person["password"], username: person["username"] });
+  }
+
+  const [person, setPerson] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  function login(){
+    console.log(person);
+    const result = props.verify(person);
+    setPerson({ email: "", password: "", username: "" });
+    console.log(result);
+  }
 
   return (
     <>
@@ -19,9 +40,9 @@ const Login = () => {
       <span className="container d-flex flex-column justify-content-center align-items-center login-center">
         
         <form className="Login col-md-8 col-lg-4 col-11">
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
-          <button type="submit">Login</button>
+          <input type="email" name="email" placeholder="Email" value={person.email} onChange={handleChange}/>
+          <input type="password" name="password" placeholder="Password" value={person.password} onChange={handleChange}/>
+          <button type="submit" onClick={login}>Login</button>
           <p>
             <Link to={"/register"}>Create Account</Link>
           </p>
