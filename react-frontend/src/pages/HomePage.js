a02a8f3770328790c46dc9ad42c367b33159866e
ChrisLiang7 <zliang15@calpoly.edu>
2022-05-09 11:38:44 -0700

    update homepage

--- react-frontend/src/pages/HomePage.js
+++ react-frontend/src/pages/HomePage.js
@@ -1,17 +1,14 @@
 import React from "react";
-import Button from '@mui/material/Button';
 import { Link } from "react-router-dom";
+import Header from "./Components/Header";
+import Imglist from "./Components/Imglist";
 
 const HomePage = () => {
   window.scrollTo(0, 0);
   return (
     <div>
-      {<Button variant="contained">Hello World</Button>
-      /* <Header />
-      <ShopSection />
-      <CalltoActionSection />
-      <ContactInfo />
-      <Footer /> */}
+      <Header />
+      <Imglist />
     </div>
   );
 }
