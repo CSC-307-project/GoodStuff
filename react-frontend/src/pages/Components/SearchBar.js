4a3c10425bc73fc53e60c7a63ea4d712ed0a6cd5
Terrence Liu <tliu29@calpoly.edu>
2022-05-13 13:13:20 -0700

    again, removed unused-var

--- react-frontend/src/pages/Components/SearchBar.js
+++ react-frontend/src/pages/Components/SearchBar.js
@@ -11,7 +11,7 @@ export default function Search() {
         <Paper variant="outlined" sx={{ width: 1 }}>
           <img
             src="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?auto=format&w=350&dpr=2"
-            alt="description of image"
+            alt="nice"
           />
         </Paper>
       </Container>
